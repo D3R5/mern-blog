622783709339cd075d50ac9fb7520a10f54bf162
D3R5 <diegoesteban.ders@gmail.com>
2024-01-25 01:16:02 -0300

    create and connect to the database

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to the database MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(3000, () => {
-  console.log('App listening on port 3000! 📡');
+  console.log("App listening on port 3000! 📡");
 });
-
- 
