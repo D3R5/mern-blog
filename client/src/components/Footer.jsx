2c2030b7e71ef75f3b20a0667f32847b81cf87f9
D3R5 <diegoesteban.ders@gmail.com>
2024-01-27 14:32:24 -0300

    create and add the footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,111 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsYoutube,
+  BsGithub,
+  BsTwitterX,
+} from "react-icons/bs";
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                DERS
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.linkedin.com/in/diego-rivera-sprohnle-4829b2198"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Linkedin
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  DERS Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/D3R5"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="DERS blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon
+              href="https://www.facebook.com/profile.php?id=100009186926264"
+              target="_blank"
+              rel="noopener noreferrer"
+              icon={BsFacebook}
+            />
+            <Footer.Icon
+              href="https://www.instagram.com/diegooders/"
+              target="_blank"
+              rel="noopener noreferrer"
+              icon={BsInstagram}
+            />
+            <Footer.Icon
+              href="https://www.youtube.com/channel/UC1c6eGpqAl-NSn6INA1VKeg"
+              target="_blank"
+              rel="noopener noreferrer"
+              icon={BsYoutube}
+            />
+            <Footer.Icon
+              href="https://github.com/D3R5"
+              target="_blank"
+              rel="noopener noreferrer"
+              icon={BsGithub}
+            />
+            <Footer.Icon
+              href="https://twitter.com/Diegooders"
+              target="_blank"
+              rel="noopener noreferrer"
+              icon={BsTwitterX}
+            />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
